f3a78c4ec67d3db44c5d2e0ebd0da81e59cd29aa
gitadmin <gitadmin@muenai.com>
2020-06-30 09:49:58 +0800

    update

--- api/find-qa/index.js
+++ api/find-qa/index.js
@@ -1,23 +1,24 @@
 module.exports = function (context, req) {
 
-  if(context.bindings.qa){
-    let data =  {
-      "id": qa.id,
-      "username":qa.username,
-      "keyowrd": qa.keyowrd,
-      "quesiton":qa.quesiton,
-      "number_of_paper":qa.number_of_paper
-    };
-    context.res = {
-          body: JSON.stringify(data)
-          headers: {
-          'Content-Type': 'application/json'
-          }
-    }
+  var qa = context.bindings.qa
+
+  if(qa){
+        context.res = {
+              body: {
+                "id": qa.id,
+                "username":qa.username,
+                "keyowrd": qa.keyowrd,
+                "quesiton":qa.quesiton,
+                "number_of_paper":qa.number_of_paper
+              },
+              headers: {
+              'Content-Type': 'application/json'
+              }
+        };
   } else {
       context.res = {
           status: 404,
-          body : JSON.stringify("No bookmarks found"),
+          body : "No bookmarks found",
           headers: {
           'Content-Type': 'application/json'
           }
