3526a738405401b136a5909a57ed56f49f46fbe9
gitadmin <gitadmin@muenai.com>
2020-07-06 12:03:24 +0800

    update

--- api/add-qa/index.js
+++ api/add-qa/index.js
@@ -1,35 +1,35 @@
 module.exports = function (context, req) {
-    // check if it exists
-    if(context.bindings.qa){
-          context.res = {
-              status: 422,
-              body : JSON.stringify("qa already exists."),
-              headers: {
-              'Content-Type': 'application/json'
-              }
-          }
-        } else {
-        // serilize
-        let qaString = JSON.stringify({
-            id: req.body.id,
-            username: req.body.username,
-            keyowrd: req.body.keyowrd,
-            quesiton: req.body.quesiton,
-            numberOfPaper: req.body.numberOfPaper
-        });
+  // check if it exists
+  if (context.bindings.qa) {
+    context.res = {
+      status: 422,
+      body: JSON.stringify("qa already exists."),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } else {
+    // serilize
+    let qaString = JSON.stringify({
+      id: req.body.id,
+      username: req.body.username,
+      keyowrd: req.body.keyowrd,
+      quesiton: req.body.quesiton,
+      numberOfPaper: req.body.numberOfPaper,
+    });
 
-        // Write to our comos db.
-        context.bindings.newqa = qaString;
-        // Push this bookmark onto our queue for further processing.
-        context.bindings.newmessage = qaString;
-        // Tell the user all is well.
-        context.res = {
-            status: 200,
-            body : JSON.stringify("qa added!"),
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        };
-    }
-    context.done();
+    // Write to our comos db.
+    context.bindings.newqa = qaString;
+    // Push this bookmark onto our queue for further processing.
+    context.bindings.newmessage = qaString;
+    // Tell the user all is well.
+    context.res = {
+      status: 200,
+      body: JSON.stringify("qa added!"),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+  context.done();
 };
--- api/find-qa/index.js
+++ api/find-qa/index.js
@@ -1,32 +1,28 @@
 module.exports = function (context, req) {
+  var qa = context.bindings.qa;
 
-  var qa = context.bindings.qa
-
-  if(qa){
-        context.res = {
-              body: {
-                "id": qa.id,
-                "username":qa.username,
-                "keyowrd": qa.keyowrd,
-                "quesiton":qa.quesiton,
-                "number_of_paper":qa.number_of_paper
-              },
-              headers: {
-              'Content-Type': 'application/json'
-              }
-        };
+  if (qa) {
+    context.res = {
+      body: {
+        id: qa.id,
+        username: qa.username,
+        keyowrd: qa.keyowrd,
+        quesiton: qa.quesiton,
+        number_of_paper: qa.number_of_paper,
+      },
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+    };
   } else {
-      context.res = {
-          status: 404,
-          body : "No bookmarks found",
-          headers: {
-          'Content-Type': 'application/json'
-          }
-      };
+    context.res = {
+      status: 404,
+      body: JSON.stringify("No bookmarks found"),
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+    };
   }
 
-
   context.done();
-
-
 };
